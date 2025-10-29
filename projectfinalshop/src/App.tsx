051630e1485554ae39cf4404717e6882b5edd9f7
TaechitSuwannakin <s6604022620360@email.kmutnb.ac.th>
2025-10-29 15:28:41 +0700

    ลืมๆ อีกรอบ

--- projectfinalshop/src/App.tsx
+++ projectfinalshop/src/App.tsx
@@ -15,7 +15,6 @@ function App() {
               <Route path="/login" element={<LoginPage />} />
               <Route path="/Register" element={<RegisterPage/>} />
               <Route path="/SalesPage" element={<HomeSalesPage/>} />
-              <Route path="/stock" element={<StockPage />} />
           </Routes>
     </>
   )
